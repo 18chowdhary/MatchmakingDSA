914fa959476e875174e1feedb9963ac1a888b752
Kristin Aoki <kaoki@olin.edu>
2020-04-14 14:02:55 -0400

    Add return statement

--- matchmaking.py
+++ matchmaking.py
@@ -32,8 +32,10 @@ def is_match(g):
             if float(node) in [x[0] for x in items]:
                 matches.append((node,val[0]))
     matches2 = set(tuple(sorted(m)) for m in matches)
-    print(matches)
+    # print(matches)
     print(matches2)
+    return matches2
+    
 if __name__ == '__main__':
     val_graph = create_validation_graph()
     pair_match = is_match(val_graph)
